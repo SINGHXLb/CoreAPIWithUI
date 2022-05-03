5a29bd6cfd038046701483ed4e2847092d0160af
patwal <pushpendra.patwal@gmail.com>
2022-05-03 16:44:15 +0800

    useState Usage

--- tsreactproject/src/App.tsx
+++ tsreactproject/src/App.tsx
@@ -2,26 +2,24 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+const {useState} = React;
+ 
+
 function App() {
-  return (
+    const [counter, setCounter] = useState(0);
+
+    return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      </div>
+          
+            <p> {counter} </p>
+            <button onClick={() =>
+            {
+                setCounter(counter + 1)
+            }} >Click Here</button>
 
-
+        </div>
 
   );
 }
