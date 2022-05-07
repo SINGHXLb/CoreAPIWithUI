f7e0635335d5400bf82615e4b653da38529d19e3
patwal <pushpendra.patwal@gmail.com>
2022-05-08 00:09:08 +0800

    Add Material Application Bar

--- tsreactproject/src/App.tsx
+++ tsreactproject/src/App.tsx
@@ -1,20 +1,45 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+ 
+import Dashboard from './components/Dashboard'; 
+import AppBar from  './components/AppBar1'
+
+
+
+// Instantiation
 
 const {useState} = React; 
 
 function App() {
     const [counter, setCounter] = useState(0);
     return (
-        <div className="App">
-            <p>
-                {counter}
-            </p>
-            <button onClick={() => {
-                setCounter(counter + 1);
-            }
-            }> click here </button>
+        <div className="App"> 
+
+         
+             <AppBar />
+          
+
+                <p>
+                    {counter}
+                </p>
+                <button onClick={() => {
+                    setCounter(counter + 1);
+                }
+                }> click here </button>
+
+                <input
+                    type="email"
+                    className="form-control"
+                    placeholder="Enter email"
+                />
+                <Dashboard />
+            
+
+      
+            
+
         </div>
     );
 }
