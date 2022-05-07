c7e35055888a91f88c34fe033608da753afe3eba
patwal <pushpendra.patwal@gmail.com>
2022-05-08 00:01:52 +0800

    Add Material Application Bar

--- tsreactproject/src/App.tsx
+++ tsreactproject/src/App.tsx
@@ -2,28 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const {useState} = React; 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      </div>
-
-
-
-  );
+    const [counter, setCounter] = useState(0);
+    return (
+        <div className="App">
+            <p>
+                {counter}
+            </p>
+            <button onClick={() => {
+                setCounter(counter + 1);
+            }
+            }> click here </button>
+        </div>
+    );
 }
-
 export default App;
